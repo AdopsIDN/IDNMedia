75662e3ec48c04e54cf9fae5aaad9483089d6f86
AdopsIDN <egi.virgiawan@idntimes.com>
2023-11-06 11:01:39 +0700

    script refresh ad slot (sticky mobile)

---
+++ Assets/Script/Script Refresh Ad Slot.js
@@ -0,0 +1,15 @@
+<!-- Script Untuk refresh - START -->
+<script type="text/javascript">
+    var frm = window.frameElement;
+    var div = frm.parentNode;
+    
+    var elemScript = document.createElement('script');
+    elemScript.innerHTML = "googletag.cmd.push(function() {googletag.pubads().addEventListener('impressionViewable',function(event) {var slot=event.slot;var a = slot.getTargeting('pos').indexOf('Sticky');if(a>-1) {setTimeout(function() {googletag.pubads().refresh([slot]); document.getElementByClass('bottom-sticky-ads').innerHTML=''; console.log('refresh')}, 30000);}});});";
+    
+    div.appendChild(elemScript);
+    </script>
+    <!-- Script Untuk refresh - END -->
+
+
+
+    
